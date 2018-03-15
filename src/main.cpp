c17abe5def4a2b0eeaee745d4139ea6b53c15020
oliver <olivercgrant@gmail.com>
2018-03-15 13:31:18 +0100

    Created a tests folder from which to run. Removed need for inputs and outputs folder, will read/write from same folder from now on, to make it cleaner for Xingran

--- src/main.cpp
+++ src/main.cpp
@@ -33,13 +33,12 @@ void AttachGlycansToGlycosites(Assembly *glycoprotein, GlycosylationSiteVector *
 
 int main(int argc, char* argv[])
 {
-    std::string working_Directory = Find_Program_Working_Directory(); // Default behaviour
+    std::string working_Directory = Find_Program_Working_Directory(); // Default behaviour. Assumes folder called glycans/ is present.
     if (argc == 2)
     {
         working_Directory = argv[1];
     }
     std::cout << "Working directory is " << working_Directory << "\n";
-
 
     //************************************************//
     // Read input file                                //
@@ -54,7 +53,7 @@ int main(int argc, char* argv[])
     // Load Protein PDB file                          //
     //************************************************//
 
-    Assembly glycoprotein( (working_Directory + "/inputs/" + proteinPDB), gmml::InputFileType::PDB);
+    Assembly glycoprotein( (working_Directory + "/" + proteinPDB), gmml::InputFileType::PDB);
     glycoprotein.BuildStructureByDistance();
 
     //************************************************//
@@ -68,7 +67,7 @@ int main(int argc, char* argv[])
     //************************************************//
 
     PdbFileSpace::PdbFile *outputPdbFileGlycoProteinAll = glycoprotein.BuildPdbFileStructureFromAssembly(-1,0);
-    outputPdbFileGlycoProteinAll->Write(working_Directory + "/outputs/GlycoProtein.pdb");
+    outputPdbFileGlycoProteinAll->Write(working_Directory + "/GlycoProtein_Initial.pdb");
     // Add beads. They make the overlap calculation faster.
     Add_Beads(&glycoprotein, &glycoSites);
     // This is where the overlaps will be resolved.
@@ -77,7 +76,7 @@ int main(int argc, char* argv[])
     Remove_Beads(glycoprotein); //Remove beads and write a final PDB & PRMTOP
 
     outputPdbFileGlycoProteinAll = glycoprotein.BuildPdbFileStructureFromAssembly(-1,0);
-    outputPdbFileGlycoProteinAll->Write(working_Directory + "/outputs/GlycoProtein_Resolved.pdb");
+    outputPdbFileGlycoProteinAll->Write(working_Directory + "/GlycoProtein_Resolved.pdb");
 
     std::cout << "Program got to end ok" << std::endl;
     return 0;
@@ -145,7 +144,7 @@ void AttachGlycansToGlycosites(Assembly *glycoprotein, GlycosylationSiteVector *
 void Read_Input_File(GlycosylationSiteVector *glycoSites, std::string *proteinPDB, std::string *glycanDirectory, const std::string working_Directory)
 {
     std::string buffer;
-    std::ifstream inf (working_Directory + "/inputs/" + "input.txt");
+    std::ifstream inf (working_Directory + "/input.txt");
     if (!inf)
     {
         std::cerr << "Uh oh, input file could not be opened for reading!" << std::endl;
@@ -160,7 +159,7 @@ void Read_Input_File(GlycosylationSiteVector *glycoSites, std::string *proteinPD
         if(strInput == "Glycans:")
         {
             getline(inf, *glycanDirectory);
-            *glycanDirectory = working_Directory + "/inputs/" + *glycanDirectory;
+            *glycanDirectory = working_Directory + "/" + *glycanDirectory;
         }
         if(strInput == "Protein Residue, Glycan Name:")
         {
