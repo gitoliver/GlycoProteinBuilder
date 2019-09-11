bec07738221a4f48201d2729df32f9faafb0e7fd
oliver <olivercgrant@gmail.com>
2019-09-11 09:17:43 +0200

    Think this was just a typo

--- includes/selections.h
+++ includes/selections.h
@@ -10,9 +10,9 @@ namespace selection
 {
 AtomVector AtomsWithinDistanceOf(Atom *query_atom, double distance, AtomVector atoms);
 void FindAtomsConnectingResidues(Atom *current_atom, Residue *second_residue, AtomVector *connecting_atoms, bool *found_neighbor);
-// pass pointer by reference in *&cycle_point, as I need to modify the actual pointer and not a copy of it.
 //void FindAtomsInPathToCycle(Atom *previous_atom, Atom *current_atom, AtomVector *atom_path, bool *found_cycle_point, Atom *&cycle_point);
 //void FindAtomsInPathToBackboneNAtom(Atom *previous_atom, Atom *current_atom, AtomVector *atom_path, bool *found_N_atom);
+//Pass pointer by reference in *&cycle_point, as I need to modify the actual pointer and not a copy of it.
 bool FindCyclePoint(Atom *previous_atom, Atom *current_atom, AtomVector *atom_path, bool *found_cycle_point, Atom *&cycle_point);
 bool FindPathBetweenTwoAtoms(Atom *current_atom, Atom *target_atom, AtomVector *atom_path, bool *found);
 //bool CheckIfCycle(Atom *previous_atom, Atom *current_atom, AtomVector *atom_path);
